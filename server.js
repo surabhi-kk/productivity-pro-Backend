922c4f834757c9766de49027b3f257873299b024
surabhi-kk <surabhikk944@gmail.com>
2026-01-06 09:31:30 +0530

    solved

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieparser());
 app.use(cors({
     origin: ["http://localhost:5173",
-        "http://localhost:5174",
+        "http://localhost:5174"
     ]
     credentials: true,
 }));
